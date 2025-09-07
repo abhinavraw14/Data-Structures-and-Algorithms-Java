99469b395e0f361634ae5c09449421d0391b6ee9
abhinavraw14 <abhinavrawat1401@gmail.com>
2025-09-07 13:33:32 +0530

    add function acccessCell()

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -5,13 +5,15 @@ public class Main {
     TwoDimensionalArray sda = new TwoDimensionalArray(3, 3);
 
     sda.insertValueInTheArray(0, 0, 10);
-    //sda.insertValueInTheArray(0, 0, 20);
-    for(int i=0; i<sda.arr.length; i++){
-      for(int j=0; j<sda.arr[0].length; j++){
-        System.out.print(sda.arr[i][j]+ " ");
-      }
-      System.out.println();
-    }
+    sda.insertValueInTheArray(0, 1, 20);
+    sda.accessCell(0, 1);
+    // //sda.insertValueInTheArray(0, 0, 20);
+    // for(int i=0; i<sda.arr.length; i++){
+    //   for(int j=0; j<sda.arr[0].length; j++){
+    //     System.out.print(sda.arr[i][j]+ " ");
+    //   }
+    //   System.out.println();
+    // }
 
 
 
