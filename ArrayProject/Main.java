c0136faa4cc8547ba6cdf61be9b7fa6bbc67f8ce
abhinavraw14 <abhinavrawat1401@gmail.com>
2025-09-07 19:36:37 +0530

    add Arrays to the project

--- ArrayProject/Main.java
+++ ArrayProject/Main.java
@@ -5,16 +5,28 @@ public class Main {
     Scanner console = new Scanner(System.in);
     System.out.print("How many days' temperature? ");
     int numDays = console.nextInt();
+
+    int [] temps = new int [numDays];
+    // record temperature and find average 
     int sum =0;
-    for(int i=1; i<=numDays; i++){
-      System.out.print("Day " + i+ "'s high temp: " );
-      int next = console.nextInt();
-      sum+=next;
+    for(int i=0; i<numDays; i++){
+      System.out.print("Day " + (i + 1) + "'s high temp: " );
+      temps[i] = console.nextInt();
+      sum+=temps[i];
     }
     double average = sum / numDays;
+
+    // count days above average temperature
+    int above = 0;
+    for(int i=0; i<temps.length; i++){
+      if(temps[i] > average){
+        above++;
+      }
+    }
     System.out.println();
     System.out.println("Average Temperature: " + average);
-    
+    System.out.println("Number of days above Average Temperature: " + above);
+
     
   }
   
